29d046790b627cc986b3d6adffb8783c9a18ac12
TimGrimbergen <timgrimbergen@hotmail.com>
2023-09-30 16:23:55 +0200

    added research proposal

--- online_testing_scripts/test_online.py
+++ online_testing_scripts/test_online.py
@@ -7,13 +7,14 @@ import numpy as np
 from algorithms.online import qThresholdOnline
 from utility_functions import generate_test_instances, test_online, plot_data
 
-N = 100
+N = 1000
 
 p_min = 1
 p_max = 10
 
 instances = generate_test_instances(N=N, n=1, m=10, s=1, h=0, p=(p_min, p_max))
 
-data = test_online(instances, qThresholdOnline(0.3, p_max))
+
+data = test_online(instances, qThresholdOnline(np.sqrt(1/p_max), p_max))
 
 plot_data(data, '../figures', 'qThreshold')
