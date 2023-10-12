a4167292e1084b716018b4c01a70add35e6ed896
Jesse <jpflikweert@gmail.com>
2023-10-12 12:31:04 +0200

    Updated terminology and structure of offline algorithm to match what's described in the paper.

--- algorithms/offline.py
+++ algorithms/offline.py
@@ -1,28 +1,34 @@
 import numpy as np
 
-# returns tuple ['schedule array', 'total_price']
+# function as defined in paper, see Lemma 1.
+def total(j, p, h):
+    if (j == 0):
+        return p[j]
+    else:
+        return p[j] + np.sum(h[0:j])
+
+# returns tuple ['schedule array', 'total_cost']
+# terminology is the same as what's used in the paper
 def solve_offline(n, m, s, p, h):
-    h_cum = np.cumsum(h)
-    p_real = [p[0]] + [p[i] + h_cum[i-1] for i in range(1,m)]
-    
-    day_seats_prices = sorted([(i, s[i], p_real[i]) for i in range(m)], key = lambda x: -x[2])
+    total_price = [(i, s[i], total(i, p, h)) for i in range(m)]
+    best_days = sorted(total_price, key = lambda x: -x[2])
     
     day_send = [0 for _ in range(m)]
-    total_price = 0
+    total_cost = 0
+    n_remaining = n
+    # iterate over best days, using Lemma 2 and Lemma 3.
+    while n_remaining > 0:
+        day, seats, price = best_days.pop()
+        seats_used = max(min(n_remaining,seats), 0)
+        day_send[day] = seats_used
+        n_remaining -= seats_used
+        total_cost += seats_used * price
     
-    new_n = n
+    # return data conforming the requested format.
+    schedule_array = [[0,0] for _ in range(m)]
+    n_remaining = n
+    for day,seats_used in enumerate(day_send):
+        schedule_array[day] = [seats_used, n_remaining-seats_used]
+        n_remaining -= seats_used
     
-    while new_n > 0:
-        day, seats, price = day_seats_prices.pop()
-        send = min(new_n,seats)
-        total_price += send * price
-        day_send[day] = send
-        new_n -= send
-    
-    ans = [[0,0] for _ in range(m)]
-    
-    for i,x in enumerate(day_send):
-        ans[i] = [x, n-x]
-        n -= x
-    
-    return [ans, total_price]
+    return [schedule_array, total_cost]
