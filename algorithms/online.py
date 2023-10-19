0e23976ec904b4f56346e936613e0f973537124a
Timo Post <timo.post@student.uva.nl>
2023-10-19 12:48:33 +0200

    Updated classes

--- algorithms/online.py
+++ algorithms/online.py
@@ -1,123 +1,92 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Iterator, Iterable
+from dataclasses import dataclass
 
-
+@dataclass
 class StrikeInstance:
-    def __init__(self, n: int, m: int, s: Iterable[int],
-                 p: Iterable[int], h: Iterable[int]) -> None:
-        """
-        Inputs:
-            n: number of people
-            m: number of days
-            s: number of seats available on each day
-            p: plane ticket price on each day
-            h: hotel price on each day
-        """
-        assert n > 0, "n must be positive"
-        assert m > 0, "m must be positive"
-        assert len(s) == len(p) == len(h) == m, "s, p, h must have length m"
-        self.__n = n; self.__m = m
-        self.__s = tuple(s)
-        self.__p = tuple(p)
-        self.__h = tuple(h)
+    n: int  # Number of people
+    m: int  # Number of days
+    s: list[int]  # Number of seats on day i
+    p: list[int]  # Price of a seat on day i
+    h: list[int]  # Hotel cost on day i
 
-    @property
-    def n(self) -> int:
-        return self.__n
+    def __post_init__(self):
+        assert self.n >= 1, "n must be at least 1"
+        assert self.m >= 1, "m must be at least 1"
+        assert len(self.s) == len(self.p) == len(self.h) == self.m, "s, p, h must have length m"
+        assert all(p_i >= 1 for p_i in self.p), "p[i] must be at least 1"
+        assert all(s_i >= 1 for s_i in self.s), "s[i] must be at least 1"
+        assert all(h_i >= 0 for h_i in self.h), "h[i] must be at least 0"
+        assert sum(self.s) >= self.n, "sum(s) must be at least n"
 
-    @property
-    def m(self) -> int:
-        return self.__m
-
-    @property
-    def s(self) -> list[int]:
-        return list(self.__s)
-
-    @property
-    def p(self) -> list[int]:
-        return list(self.__p)
-
-    @property
-    def h(self) -> list[int]:
-        return list(self.__h)
-
-    def __len__(self) -> int:
-        return self.m
-
-    def __iter__(self) -> Iterator[tuple[int, int, int]]:
-        for i in range(self.m):
-            yield self.__s[i], self.__p[i], self.__h[i]
+    @classmethod
+    def from_file(cls, file: str) -> StrikeInstance:
+        with open(file, 'r') as f:
+            n = int(f.readline())
+            m = int(f.readline())
+            s, p, h = [], [], []
+            for _ in range(m):
+                s_i, p_i, h_i = f.readline().split(', ')
+                s.append(int(s_i))
+                p.append(int(p_i))
+                h.append(int(h_i))
+        return cls(n, m, s, p, h)
 
 
 class OnlineAlgorithm(ABC):
-    __algorithm_name = "Online Algorithm"
-
-    @property
-    def algorithm_name(self) -> str:
-        return self.__algorithm_name
-
     @classmethod
-    def __subclasshook__(cls, subclass):
+    def __subclasshook__(cls, subclass: OnlineAlgorithm):
         return (hasattr(subclass, 'setup') and
                 callable(subclass.setup) and
                 hasattr(subclass, 'decide') and
-                callable(subclass.decide) or
+                callable(subclass.decide) and
+                hasattr(subclass, 'instance') or
                 NotImplemented)
 
-    def __init__(self, instance: StrikeInstance) -> None:
+    def __init__(self, instance: StrikeInstance, *args, **kwargs) -> None:
         self.__instance = instance
-        self.__cost = 0
-        self.__i = 0
-        self.__f = []
-        self.__r = []
+        self.setup(*args, **kwargs)
 
     @property
     def instance(self) -> StrikeInstance:
         return self.__instance
 
+    @abstractmethod
+    def setup(self, *args, **kwargs) -> None:
+        pass
+
+    @abstractmethod
+    def decide(self, i: int, n_i: int, m_i: int, s_i: int, p_i: int, h_i: int) -> int:
+        pass
+
+    @property
+    def solution(self) -> StrikeSolution:
+        n, m = self.instance.n, self.instance.m
+        s, p, h = self.instance.s, self.instance.p, self.instance.h
+        n_i = n
+        # List comprehension has better performance than initializing to zeros
+        # or using append.
+        def decide(i, s_i, p_i, h_i):
+            nonlocal n_i
+            f_i = self.decide(i, n_i, m - i, s_i, p_i, h_i)
+            n_i -= f_i
+            return f_i, n_i
+        solution = [decide(i, s_i, p_i, h_i)
+                    for i, (s_i, p_i, h_i)
+                    in enumerate(zip(s, p, h))]
+        return StrikeSolution(self.instance, solution)
+
+
+@dataclass
+class StrikeSolution:
+    instance: StrikeInstance
+    solution: list[tuple[int, int]]
+
     @property
     def cost(self) -> int:
-        if self.__i < self.instance.m:
-            self.run()
-        return self.__cost
-
-    @property
-    def f(self) -> list[int]:
-        if self.__i < self.instance.m:
-            self.run()
-        return self.__f
-
-    @property
-    def r(self) -> list[int]:
-        if self.__i < self.instance.m:
-            self.run()
-        return self.__r
-
-    def __iter__(self) -> Iterator[tuple[int, int]]:
-        for i, (s_i, p_i, h_i) in enumerate(self.instance):
-            if i < self.__i:  # already decided
-                yield self.__f[i], self.__r[i]
-            else:  # decide if not already decided
-                n_i = self.instance.n if i == 0 else self.__r[i - 1]
-                f_i = self.decide(i, n_i, s_i, p_i, h_i)
-                r_i = n_i - f_i
-                self.__f.append(f_i)
-                self.__r.append(r_i)
-                self.__cost += f_i * p_i + r_i * h_i
-                self.__i += 1
-                yield f_i, r_i
-
-    def run(self) -> None:
-        for _ in self:
-            pass
-
-    @abstractmethod
-    def setup(self) -> None:
-        pass
-
-    @abstractmethod
-    def decide(self, i: int, n_i: int, s_i: int, p_i: int, h_i: int) -> int:
-        pass
+        return sum(f_i * p_i + h_i * r_i
+                   for (f_i, r_i), p_i, h_i
+                   in zip(self.solution, self.instance.p, self.instance.h))
 
 
 class qThresholdOnline(OnlineAlgorithm):
