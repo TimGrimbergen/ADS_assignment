6a7a89ce7c7d115292d1202bdde2edbfda739c89
Agostino Sorbo <agostino.sorbo@gmail.com>
2023-10-24 20:54:44 +0200

    add violin plot, run test until convergence of the random algorithm, remove zeros from plot data

--- test_all.py
+++ test_all.py
@@ -5,7 +5,7 @@ sys.path.insert(1, os.path.dirname(os.path.dirname(__file__))) # needed to acces
 
 import numpy as np
 from algorithms.online import QThreshold, Random
-from utility_functions import generate_test_instances, plot_data, test_online_until_avgcase
+from utility_functions import generate_test_instances, plot_data, test_online_until_converged
 
 # use this script to automate testing multiple algorithms.
 
@@ -16,8 +16,10 @@ p_max = 100
 
 instances = list(generate_test_instances(N=N, n=1, m=10, s=1, h=0, p=(p_min, p_max)))
 
-data = test_online_until_avgcase(N, instances, QThreshold, 1/np.sqrt(p_max))
-plot_data(data, 'figures', 'qThreshold')
+qThreshold_data = test_online_until_converged(N, instances, QThreshold, 1/np.sqrt(p_max))
+plot_data('figures', 'qThreshold', qThreshold_data)
 
-data = test_online_until_avgcase(N, instances, Random)
-plot_data(data, 'figures', 'RandomOnline')
+RandomOnline_data = test_online_until_converged(N, instances, Random)
+plot_data('figures', 'RandomOnline', RandomOnline_data)
+
+plot_data('figures', 'violin', qThreshold_data, RandomOnline_data)
