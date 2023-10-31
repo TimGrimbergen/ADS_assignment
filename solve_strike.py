2fe38fea2c58204be545dbd53a7b850236f011d1
Jesse <jpflikweert@gmail.com>
2023-10-31 14:18:01 +0100

    Bugfixes to test_one

--- solve_strike.py
+++ solve_strike.py
@@ -7,10 +7,9 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python3 offline.py <instance>")
         sys.exit(1)
-    I = Instance.from_file(sys.argv[1])
+    I = Instance.from_file(f"input/{sys.argv[1]}")
     for flying, staying in offline(I):
         print(f"{flying}, {staying}")
-
 
 if __name__ == '__main__':
     main()
